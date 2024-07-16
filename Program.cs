ad82d3c733908dc7d47e0b10061f4564becff15f
The-Leon <user@WIN-A6H3AK64JSB>
2024-07-16 22:19:31 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using PhoneBookWithFile.Services;
+using System;
+using System.Runtime.InteropServices;
+
+namespace PhoneBookWithFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileService fileservice = new FileService();
+            LoggingService logger = new LoggingService();
+
+            Console.WriteLine("Welcome to our Phone-Book project !!!");
+
+            bool isExit = true;
+            try
+            {
+                while (isExit)
+                {
+                    logger.LoggerMenu();
+                    string userChoose = Console.ReadLine();
+
+                    switch (userChoose)
+                    {
+                        case "1":
+                            fileservice.ReadFile().ToList().Sort();
+                            logger.LoggerForAdd();
+                            string nameAndNumber = Console.ReadLine();
+                            fileservice.AddNameAndNumber(nameAndNumber);
+                            nameAndNumber = "";
+                            break;
+                        case "2":
+                            fileservice.ReadFile().ToList().Sort();
+                            logger.LoggerForRemove();
+                            nameAndNumber = Console.ReadLine();
+                            fileservice.RemoveNameAndNumber(nameAndNumber);
+                            break;
+                        case "3":
+                            fileservice.ReadFile().ToList().Sort();
+                            break;
+                        case "4":
+                            Console.WriteLine("Warning... fileni o'chirishga rozimisan : yes/no");
+                            string chooseYesOrno = Console.ReadLine();
+                            if (chooseYesOrno.ToLower() == "yes")
+                            {
+                                fileservice.ClearFile();
+                            }
+                            else
+                            {
+                                Console.Write("Fileni o'chirmadingmi  ? ");
+                            }
+                            break;
+                        case "5":
+                            isExit = false;
+                            break;
+                        default:
+                            Console.WriteLine("");
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LoggerExcepion(ex.Message);
+            }
+        }
+    }
+}
