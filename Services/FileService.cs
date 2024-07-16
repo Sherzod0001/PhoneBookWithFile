a9348fa3be2d86d8e835ddf482382a052f4e970b
The-Leon <user@WIN-A6H3AK64JSB>
2024-07-16 22:19:31 +0500

    Add project files.

---
+++ Services/FileService.cs
@@ -0,0 +1,67 @@
+﻿namespace PhoneBookWithFile.Services
+{
+    internal class FileService : IFileService
+    {
+        private const string filePath = "../../../phoneBook.txt";
+        private ILoggingService loggingService;
+
+        public FileService()
+        {
+            this.loggingService = new LoggingService();
+
+            EnsureFileExists();
+        }
+
+        public void ClearFile()
+        {
+            File.WriteAllText(filePath, string.Empty);
+        }
+
+        public string AddNameAndNumber(string nameAndNumber)
+        {
+            string line = "\n";
+            File.AppendAllText(filePath, nameAndNumber + line);
+            
+            return nameAndNumber;
+        }
+         
+        public void RemoveNameAndNumber(string nameAndNumber)
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            string lineToRemove = nameAndNumber;
+
+            lines.Remove(lineToRemove);
+
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public void ReplaceNumberAndName(string nameAndNumber)
+        {
+            RemoveNameAndNumber(nameAndNumber);
+            AddNameAndNumber(nameAndNumber);
+
+        }
+
+        public string ReadFile()
+        {
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+            return readText;
+        }
+        private static void EnsureFileExists()
+        {
+            bool isFilePresent = File.Exists(filePath);
+
+            if (isFilePresent is false)
+            {
+                File.Create(filePath).Close();
+            }
+        }
+
+        void IFileService.AddName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/LoggingService.cs
@@ -0,0 +1,40 @@
+﻿namespace PhoneBookWithFile.Services
+{
+    internal class LoggingService : ILoggingService
+    {
+
+        public void LoggerMenu()
+        {
+            Console.WriteLine("what do you want to do");
+            Console.WriteLine("-'1'- Add a contact");
+            Console.WriteLine("-'2'- Remove a contact");
+            Console.WriteLine("-'3'- Show all contact");
+            Console.WriteLine("-'4'- Clear Phone-Book");
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-'5'- EXIT");
+            //Console.ResetColor();
+        }
+
+        public void LoggerForAdd() 
+        {
+            Console.WriteLine("Ogoxlantirish : qo'shish uchun ism va raqam formatini to'g'ri yozing: (Sherzod +998918285636)");
+            Console.Write("Ism va raqamni kiriting :");
+        } 
+
+        public void LoggerForRemove()
+        {
+            Console.WriteLine("Ogoxlantirish : o'chirish uchun ism va raqam formatini to'g'ri yozing: (Sherzod +998918285636)");
+            Console.Write("Ism va raqamni kiriting :");
+        }
+
+        public void LoggerExcepion(string message)
+        {   
+            Console.WriteLine($"{message}");
+            Console.WriteLine("Run again");
+        }
+        public void LogInformation(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
